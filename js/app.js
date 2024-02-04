40082ac098a4852639a09770092012ff767404a1
ArtMen4ik <ArtMen4ik00@gmail.com>
2024-02-04 22:08:42 +0700

    коммит 1

---
+++ js/app.js
@@ -0,0 +1,6 @@
+let a = parseFloat(prompt("введте А"));
+let b = parseFloat(prompt("введте B"));
+a = a + b;
+b = a - b;
+a = a - b;
+alert("A = " + a + " B = " + b);
---
+++ js/app2.js
@@ -0,0 +1,3 @@
+let sec_count = parseInt(prompt("введите количество секунд с начала суток"))
+let hour = sec_count / 3600
+alert("с начала суток прошло " + sec_count + " секунд, что в часах = " + parseInt(hour))
